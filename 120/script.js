0e69bfebacc93b51535732859c97b9e05bf5fb6d
nevatiarishabh <42384620+nevatiarishabh@users.noreply.github.com>
2020-07-29 11:39:32 +0530

    Add files via upload

---
+++ 120/script.js
@@ -0,0 +1,5 @@
+var firstname = prompt("Enter your first name:");
+var lastname = prompt("Enter your last name:");
+var age = prompt("Enter your age :");
+console.log("The full name entered is: "+ firstname+" "+lastname);
+console.log("The age is "+age+" years old.");
